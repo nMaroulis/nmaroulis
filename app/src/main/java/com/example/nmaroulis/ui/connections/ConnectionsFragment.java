9f9a89b0b5db7101b465d272a02c9fc6470a72ff
Nikos Maroulis <nick.maroulis@hotmail.com>
2021-09-25 20:16:15 +0200

    final

--- app/src/main/java/com/example/nmaroulis/ui/connections/ConnectionsFragment.java
+++ app/src/main/java/com/example/nmaroulis/ui/connections/ConnectionsFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SearchView;
 import android.widget.SimpleAdapter;
@@ -52,7 +53,10 @@ public class ConnectionsFragment extends Fragment {
     private User[] connections;
     private User[] pending_connections;
     private User[] connection_requests;
-
+    ArrayList<String> items=new ArrayList<>();
+    ArrayList<String> user_ids=new ArrayList<>();
+    SpinnerDialog spinnerDialog;
+    private Button showConnectionsButton;
     private SearchView searchView;
 
 
@@ -73,28 +77,92 @@ public class ConnectionsFragment extends Fragment {
             }
         });
 
-        SearchManager searchManager = (SearchManager) cxt.getSystemService(Context.SEARCH_SERVICE);
-        searchView =binding.searchUsers;
-        searchView.setSearchableInfo(searchManager.getSearchableInfo(this.getActivity().getComponentName()));
+        showConnectionsButton = binding.showConnectionsButton;
 
-        //        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-        //            @Override
-        //            public boolean onQueryTextSubmit(String query) {
-        //                connectionsTagAdapter.filter(query);
-        //                return true;
-        //            }
-        //
-        //            @Override
-        //            public boolean onQueryTextChange(String newText) {
-        //                connectionsTagAdapter.filter(newText);
-        //                return true;
-        //            }
-        //        });
 
+
+//        items.add("Mumbai");
+//        items.add("Delhi");
+//        items.add("Bengaluru");
+//        items.add("Hyderabad");
+//        items.add("Ahmedabad");
+//        items.add("Chennai");
+//        items.add("Kolkata");
+//        items.add("Surat");
+//        items.add("Pune");
+//        items.add("Jaipur");
+//        items.add("Lucknow");
+//        items.add("Kanpur");
 
         SharedPreferences pref = cxt.getSharedPreferences("User_info", 0);
         jwt_token = pref.getString("jwt_token", null); // fortwma tou jwt token
         uid = pref.getInt("user_id", -1); // fortwma tou user id
+
+        RequestQueue queue = Volley.newRequestQueue(cxt);
+        Gson gson = new Gson();
+        String url ="http://10.0.2.2:8080/user_search/" + uid.toString();  // new post url
+
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new com.android.volley.Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+
+                        User[] con_search = gson.fromJson(response, User[].class);
+                        Log.d("Conn Search :: User","Response is: "+ response);
+
+                        for(int i=0;i<con_search.length;i++){
+                            items.add(con_search[i].getFullName());
+                            user_ids.add(Integer.toString(con_search[i].getId()));
+                        }
+
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d("Conn Search RES","That didn't work!");
+            }
+        }) {
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", jwt_token);
+                return params;
+            }
+        };
+        queue.add(stringRequest);
+
+        spinnerDialog = new SpinnerDialog(getActivity(), items,
+                "Αναζήτηση Χρηστών", user_ids);
+
+        spinnerDialog.setTitleColor(getResources().getColor(R.color.black));
+        spinnerDialog.setSearchIconColor(getResources().getColor(R.color.black));
+        spinnerDialog.setSearchTextColor(getResources().getColor(R.color.black));
+        spinnerDialog.setItemColor(getResources().getColor(R.color.black));
+        spinnerDialog.setItemDividerColor(getResources().getColor(R.color.purple_700));
+        spinnerDialog.setCloseColor(getResources().getColor(R.color.black));
+
+        spinnerDialog.setCancellable(true);
+        spinnerDialog.setShowKeyboard(false);
+
+        spinnerDialog.bindOnSpinerListener(new OnSpinerItemClick() {
+            @Override
+            public void onClick(String item, int position, String connection_id) {
+
+                Toast.makeText(getActivity(), item + "  " + position + "", Toast.LENGTH_SHORT).show();
+                Log.d("search :: User","Response is: "+ item + " " + position + " " + connection_id);
+
+                sendConnectionRequest(jwt_token, uid, connection_id, root);
+            }
+        });
+
+        showConnectionsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                spinnerDialog.showSpinerDialog();
+            }
+        });
+
+
 
         getProfileConnections(jwt_token, uid, root);
         getProfileRequests(jwt_token, uid, root);
@@ -239,5 +307,53 @@ public class ConnectionsFragment extends Fragment {
         };
         queue.add(stringRequest);
     }
+
+
+    public void sendConnectionRequest(String jwt_token, Integer uid, String connection_id, View root){
+
+        RequestQueue queue = Volley.newRequestQueue(cxt);
+        Gson gson = new Gson();
+        String url ="http://10.0.2.2:8080/user/" + uid.toString() + "/request/" + connection_id;  // new request url
+
+        HashMap<String, String> newrequest_data = new HashMap();
+        JSONObject newpost_data_json = new JSONObject(newrequest_data);
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
+                (Request.Method.POST, url,newpost_data_json ,
+                        new Response.Listener<JSONObject>() {
+                            @Override
+                            public void onResponse(JSONObject response) {
+
+                                Log.d("New Request ::","Response is: " + response.toString());
+
+                                String welcome = "Το αίτημα σας στάλθηκε επιτυχώς";
+                                Toast.makeText(cxt, welcome, Toast.LENGTH_LONG).show();
+
+                                Intent intent = getActivity().getIntent();
+                                getActivity().finish();
+                                startActivity(intent);                                    }
+                        }, new Response.ErrorListener() {
+
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // TODO: Handle error
+                        Log.d("NewRequest :: Error","Error is: " + newpost_data_json.toString());
+
+                        Toast.makeText(cxt, "Το αίτημα σας δεν στάλθηκε", Toast.LENGTH_LONG).show();
+
+                    }
+                }){
+            @Override
+            public Map<String, String> getHeaders() throws AuthFailureError {
+                Map<String, String> params = new HashMap<String, String>();
+                params.put("Authorization", jwt_token);
+                params.put("Content-Type", "application/x-www-form-urlencoded");//"application/json");
+                return params;
+            }
+        };
+        queue.add(jsonObjectRequest);   // Add the request to the RequestQueue.
+
+    }
+
+
 
 }
---
+++ app/src/main/java/com/example/nmaroulis/ui/connections/OnSpinerItemClick.java
@@ -0,0 +1,7 @@
+package com.example.nmaroulis.ui.connections;
+
+
+public interface OnSpinerItemClick
+{
+    public void onClick(String item,int position, String uid);
+}
